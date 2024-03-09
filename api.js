baf81a085ccd57d40b1d29b0c0120353e9c2ee6d
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2024-03-09 19:03:44 +0000

    Cria rota 'turmas' vazia no back end.

--- api.js
+++ api.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const cors = require('cors');
 
+const turmasRouter = require('./api/turmas');
+
 const API_PORT = 3000;
 const API_PATH = 'api';
 
 const app = express();
 app.use(cors());
+
+app.use(`/${API_PATH}/turmas`, turmasRouter);
 
 app.listen(API_PORT, () => {
   console.log(
